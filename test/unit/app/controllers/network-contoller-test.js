9fce5534a196b2a6ac0656486a1d056cf1a0d39f
Creature101 <dev.kononov@gmail.com>
2019-07-25 09:13:00 +0300

    FUNC

--- test/unit/app/controllers/network-contoller-test.js
+++ test/unit/app/controllers/network-contoller-test.js
@@ -91,7 +91,7 @@ describe('Network utils', () => {
         expected: 'Kovan',
       }, {
         input: 'mainnet',
-        expected: 'Main Ethereum Network',
+        expected: 'Main FUNCOIN Network',
       }, {
         input: 'goerli',
         expected: 'Goerli',
